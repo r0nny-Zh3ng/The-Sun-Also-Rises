a11978d9910df11f901ba10531217bd29732909a
r0nny-Zh3ng <89795177+r0nny-Zh3ng@users.noreply.github.com>
2022-10-24 12:57:38 -0400

    Add files via upload

---
+++ DeeplyNestedObject/js/site.js
@@ -0,0 +1,77 @@
+const theSun = {
+    names: [
+        Sun,
+        Sol,
+        Helios,
+    ],
+    starType: "yellow Dwarf",
+
+    sunStructure:[
+        core,
+        radiativeZone,
+        tachocline,
+        convectiveZone,
+        Flare,
+        Chromosphere,
+        corona,
+        prominence,
+        solarWind,
+    ],
+
+    obsevationData: {
+    meanDistanceFromEarth: "1 AU",
+    visualBrightness: "-26.74 V",
+    absoluteMagnitude: "4.83",
+    spectralClassification: "G2V",
+    metallicity: "Z = 0.0122",
+    angularSize: "31.6-32.7 minutes of arc/ 0.527-0.545 degrees", },
+
+
+    orbitalCharacteristics:{
+    meanDistanceFromMilkyWayCore:"≈2.7*10^17 km",
+    galacticPeriod:"(2.25 - 2.50)*10^8 yr",
+    velocity:"≈251km/s",},
+
+    physicalCharacteristics:{
+    equatorialRadius: "696342 km",
+    equatorialCircumference: "4.379*10^6 km",
+    flattening: "9*10^(-6) km",
+    surfaceArea: "6.09*10^12 km",
+    volume:"1.41 * 10^18 km",
+    mass: "1.9885 * 10^30 kg",
+    averageDensity: "1.4908 g/cm^3",
+    centerDensity: "162.2 g/cm^3",
+    equatorialSurfaceGravity: "274 m/s",
+    momentOfInertiaFactor: "0.070",
+    escapeVelocity: "617.7 km/s",
+    centerTemperature: "1.57 * 10^7 K",
+    photosphereTemperature: "5772 K",
+    coronaTemperature: "≈ 5 * 106 K",
+    luminosity: "3.828 * 10^26 W",
+    color: "0.63",
+    meanRadiance: "2.009 * 10^7 W·m^(−2)·sr^(−1)",
+    age: "≈4.6 billion years",
+    
+    photosphericComposition: [
+        73.56% hydrogen,
+        24.85% helium,
+        0.77% oxygen,
+        0.29% carbon,
+        0.16% iron, 
+        0.12% neon,
+        0.09% nitrogen,
+        0.07% silicon,
+        0.05% magnesium,
+        0.04% sulphur,
+    ],},
+
+    rotationCharacteristics:{
+    obliquityToTheEcliptic: "7.25º",
+    obliquityToTheGalacticPlane: "67.23º",
+    rightAscensionOfNorthPole: "286.13º/ 19h4min30s",
+    declinationOfNorthPole:"+63.87º/ 63º52'North",
+    sideralRotationPeriod: "25.05 days at equator/ 25.38 days at 16º latitude/ 34.4 days at poles",
+    rotationVelocity: "1.997 km/s",}
+
+
+}
